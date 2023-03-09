23bac91e20f625d700b3291bc5d196a429dfd716
David Čech <cechdav5@fel.cvut.cz>
2023-03-09 19:50:47 +0100

    Added tests for sum class

---
+++ src/test/java/SumTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SumTest {
+
+    @Test
+    public void sumRecursive_sumOfFour_10() {
+        Assertions.assertEquals(10, Sum.sumRecursive(4));
+    }
+
+    @Test
+    public void sumIterative_sumOfSix_21() {
+        Assertions.assertEquals(21, Sum.sumIterative(6));
+    }
+
+}
